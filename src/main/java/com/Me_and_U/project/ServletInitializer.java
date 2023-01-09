e7fc840611a48fd24a5d70083f0d40d36c27c618
sidhd3723 <kg3723@naver.com>
2023-01-09 14:36:59 +0900

    no message

---
+++ src/main/java/com/Me_and_U/project/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.Me_and_U.project;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MeAndUApplication.class);
+	}
+
+}
---
+++ src/test/java/com/Me_and_U/project/MeAndUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Me_and_U.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeAndUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
