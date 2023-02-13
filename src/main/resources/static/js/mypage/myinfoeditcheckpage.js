361d2f19607aeff364fb84c7595aba4f5bed8cf8
ruddks <ruddks2222@gmail.com>
2023-02-13 15:06:10 +0900

    마이페이지

---
+++ src/main/resources/static/js/mypage/myinfoeditcheckpage.js
@@ -0,0 +1,60 @@
+/**
+ * myinfoeditpage.js
+ */
+ 
+ $(document).ready(function(){
+	$('#checkPwd').on('click',function() {
+	event.preventDefault();
+		$.ajax({
+			type: "post",
+			url: "/myinfoeditcheckpage",
+			data: {
+				"pw": $('#pw').val()
+			},
+			dataType:'text',
+			success:function(result){
+				if(result == "success"){
+					location.href="/myinfoeditpage";
+				} else {
+					alert("비밀번호가 일치하지 않습니다.");
+				}
+			}
+		});	// ajax 종료
+	});
+	
+ $cateBtn = $(".cateBtn");
+	$cateBtn.each(function(index) {
+		var search = ["전체", "여행", "운동", "게임", "문화", "음악","기타"];
+		$('#categ' + index).on('click', function() {
+			if(search[index - 1] == "전체") {
+				$(".review_container").show();
+			} else {
+				$(".review_container").hide();
+				$('.review_container[value*=' + search[index - 1] + ']').show();
+			}
+		});
+	});
+	
+	$("#myp").click(function() {
+            location.href="/mypage";
+    });
+	$("#infoedit").click(function() {
+            location.href="/myinfoeditcheckpage";
+    });
+    $("#myreiew").click(function() {
+            location.href="/myreviewpage";
+    });
+    $("#selectreg").click(function() {
+            location.href="/myselectregpage";
+    });
+  
+  
+	// 로그인 성공 시 페이지
+
+	
+
+
+
+
+});
+
